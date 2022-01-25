e86194dd92989981318caa166b4e7d83f52cd04b
iBoBoTi <iroegbucalistus@gmail.com>
2022-01-25 20:15:19 +0100

    fix: add the env loader package godotenv

--- Documents/GOWORKSPACE/src/github.com/iBoBoTi/CMS-BlogPost/cmd/main.go
+++ Documents/GOWORKSPACE/src/github.com/iBoBoTi/CMS-BlogPost/cmd/main.go
@@ -4,26 +4,34 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/iBoBoTi/CMS-BlogPost/pkg/app"
 	"github.com/iBoBoTi/CMS-BlogPost/pkg/repository"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
 	// color logger
 	gin.ForceConsoleColor()
+	er := godotenv.Load()
+	if er != nil {
+		log.Println(er.Error())
+	}
 
 	router := gin.Default()
 
-	server := app.NewServer(router)
-
 	// database connection initiated
-	db,_ := repository.DataBaseConnection()
+	db, errr := repository.DataBaseConnection()
+	if errr != nil {
+		panic(errr)
+	}
 	defer db.Close()
-
-
+	DB := repository.Storage{DB: db}
+	server := app.NewServer(router, DB)
 
 	// starts server at 8080 default for gin
 	err := server.RunServer()
 	if err != nil {
-		return 
+		log.Fatal(err.Error())
+		return
 	}
 
 }
